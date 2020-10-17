60376784edff81944249a304649056d2d9211183
alejandro Medina <alejandro.medina104@alu.ulpgc.es>
2020-10-17 14:24:36 +0100

    Kata1 version2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,14 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Hortensio",new Date(81,11,23));
+        Calendar date= GregorianCalendar.getInstance();
+        date.set(1984,6,3);
+        Person person = new Person("Hortensio",date);
         
         System.out.println(person.getName() +" tiene  "+ person.getAge());
                 
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,14 +1,16 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     
     private final String name;
-
-    private final Date birthdate;
-
-    public Date getBirthdate() {
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000 * 60 * 60 * 24 * 365.25);
+    
+    public Calendar getBirthdate() {
+        
         return birthdate;
     }
 
@@ -16,11 +18,17 @@ public class Person {
         return name;
     }
 
-    public Person(String name, Date birthdate) {
+    public Person(String name, Calendar birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
     public int getAge(){
-        return (int)((new Date().getTime() - birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int)millisecondsToYears(today.getTimeInMillis() - birthdate.getTimeInMillis());
     }
+
+    private long millisecondsToYears(long milliseconds) {
+        return milliseconds/MILLISECONDS_PER_YEAR;
+    }
+    
 }
